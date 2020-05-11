ec0451526bd4d9c498757aca757986602a2a360c
Ruben Vitt <dev@rubeen.dev>
2020-05-11 13:52:34 +0200

    init(project)

---
+++ SQL/ConstValues.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+
+namespace SQL
+{
+    public static class Const
+    {
+        public static string Host { get { return "rubeen.de"; } }
+        public static int Port { get { return 3333; } }
+        public static string User { get { return "root"; } }
+        private static string _pass = ConfigurationManager.AppSettings["SSHPassword"];
+        public static string Pass { get { return "kusapit(k3seq-k9goweqow&l"; } }
+        public static uint SqlPort { get { return 3306; } }
+        public static string SqlConn { get { return "Server=localhost; Port=3306; Database=rubeen_testdatenbank; Uid=datentester; Pwd=0sb7PmkkCl#r8Ajj"; } } 
+        public static string SqlHost { get { return "localhost"; } }
+        public static string SqlIPA { get { return "127.0.0.1"; } }
+        public static string SqlSelect { get { return "SELECT tbl_users.id FROM tbl_users"; } }
+    }
+}
---
+++ SQL/Test.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using Renci.SshNet;
+using Renci.SshNet.Common;
+using System.Data;
+using System.Net.Sockets;
+
+namespace SQL
+{
+    class Test
+    {
+        void Run()
+        {
+            PasswordConnectionInfo connectionInfo = new PasswordConnectionInfo(Const.Host, Const.Port, Const.User, Const.Pass);
+            connectionInfo.Timeout = TimeSpan.FromSeconds(30);
+            using (SshClient client = new SshClient(connectionInfo))
+            {
+                try
+                {
+                    client.Connect();
+                    if (client.IsConnected) { Console.WriteLine("SSH Connection is active."); }
+                    else { Console.WriteLine("SSH Connection failed."); }
+                    ForwardedPortLocal portFwdL = new ForwardedPortLocal(Const.SqlIPA, Const.SqlPort, Const.SqlHost, Const.SqlPort);
+                    client.AddForwardedPort(portFwdL);
+                    portFwdL.Start();
+                    if (portFwdL.IsStarted)
+                        Console.WriteLine("port forwarding is started");
+                    else Console.WriteLine("port forwarding failed.");
+
+                    string strConnection = Const.SqlConn;
+                    MySqlConnection conn = new MySqlConnection(strConnection);
+                    string myTableName = "tbl_users";
+                    MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                    myDataAdapter.SelectCommand = new MySqlCommand(Const.SqlSelect, conn);
+                    MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAdapter);
+                    try
+                    {
+                        conn.Open();
+                        Console.WriteLine("SQL connection is active.");
+                        DataSet ds = new DataSet("users");
+                        myDataAdapter.Fill(ds, myTableName);
+                        myDataAdapter.Update(ds, myTableName);
+                        Console.WriteLine(ds.GetXml());
+                    }
+                    catch(MySqlException ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                    finally { conn.Close(); }
+                    client.Disconnect();
+                    Console.WriteLine("SSH Disconnect");
+                }
+                catch(SocketException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            new Test().Run();
+            Console.ReadKey();
+        }
+    }
+}
